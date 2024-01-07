5d3b1add6427e684d32fa1d50afb8826b8182391
Junior Hirwa <iamhirwejr@gmail.com>
2024-01-07 15:47:30 +0200

    feat: add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
