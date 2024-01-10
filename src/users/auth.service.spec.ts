0f3f4aaaa806abe38ef77c821073cc7027ac30a0
Junior Hirwa <iamhirwejr@gmail.com>
2024-01-10 04:41:45 +0200

    test: test signup functionality

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,39 +1,69 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
 
 describe('AuthService', () => {
+  const user = {
+    id: 1,
+    fullName: 'foobar',
+    phone: '098934',
+    email: 'foo@bay.com',
+    password: 'password',
+  };
   let authService: AuthService;
-  let mockUsersService: Partial<UsersService>;
+  const mockUserService = {
+    createUser: jest.fn((user) => user),
+    findUserByEmail: jest.fn((email: string) =>
+      email === user.email ? [user] : [],
+    ),
+    findUserById: jest.fn((id: string) => {
+      return { id, ...user };
+    }),
+  };
 
   beforeEach(async () => {
-    const user: User = {
-      id: 1,
-      fullName: 'foobar',
-      phone: '098934',
-      email: 'foo@bar.com',
-      password: 'password',
-    };
-
-    mockUsersService = {
-      createUser: (user: User) => Promise.resolve(user),
-      findUserByEmail: (email: string) => Promise.resolve([{ email, ...user }]),
-      findUserById: (id: number) => Promise.resolve({ id, ...user }),
-    };
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
         {
           provide: UsersService,
-          useValue: mockUsersService,
+          useValue: mockUserService,
         },
       ],
     }).compile();
 
     authService = module.get(AuthService);
   });
-  it('should create an instance of auth service', () => {
-    expect(authService).toBeDefined();
+  describe('AuthService', () => {
+    it('checks if the the auth service is defined', () => {
+      expect(authService).toBeDefined();
+    });
+  });
+
+  describe('signup', () => {
+    const fakeUser = {
+      id: 1,
+      fullName: 'foobar',
+      phone: '098934',
+      email: 'foo@baek.com',
+      password: 'password',
+    };
+    it('should create a new user with a hashed and salted password', async () => {
+      const newUser = await authService.signup(fakeUser);
+      expect(newUser.password).not.toEqual(fakeUser.password);
+      const [salt, hash] = newUser.password.split('.');
+      expect(salt).toBeDefined();
+      expect(hash).toBeDefined();
+    });
+
+    it('should throw an error if the user already exists', async () => {
+      await authService.signup(fakeUser);
+
+      try {
+        await authService.signup(fakeUser);
+      } catch (err) {
+        return;
+      }
+    });
   });
 });
