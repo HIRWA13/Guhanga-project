b76f37c09a6fae60aa5ecc8b4cbbbab4e931632e
Junior Hirwa <iamhirwejr@gmail.com>
2024-01-19 09:51:45 +0200

    feat: update reviews controller

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -5,14 +5,14 @@ import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { serialize } from 'src/interceptors/serialize.interceptor';
-import { ReportDto } from './dtos/report.dto';
+import { ReviewDto } from './dtos/review.dto';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private reviewsService: ReviewsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  @serialize(ReportDto)
+  @serialize(ReviewDto)
   create(@Body() review: CreateReviewDto, @CurrentUser() user: User) {
     return this.reviewsService.create(review, user);
   }
