e013f49cd95e9afa909da222606e1708364155f1
Junior Hirwa <iamhirwejr@gmail.com>
2024-01-19 09:46:08 +0200

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import { ReviewsModule } from './reviews/reviews.module';
+import { Review } from './reviews/entities/reviews.entity';
 
 @Module({
   imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Review],
+        };
+      },
+    }),
+    ReviewsModule,
   ],
 })
 export class AppModule {}
